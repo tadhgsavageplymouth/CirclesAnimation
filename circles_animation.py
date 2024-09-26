ec9ac55c7afbc9831aeb4887dae8d16af8b2e8b8
tadhgsavageplymouth <tadhg.savage@students.plymouth.ac.uk>
2024-09-26 17:02:17 +0100

    slower growth of circles and not so much overlapping

--- circles_animation.py
+++ circles_animation.py
@@ -9,8 +9,8 @@ ax.set_xlim(-10, 10)
 ax.set_ylim(-10, 10)
 
 # Create two circles with initial small radii
-circle1 = Circle((-3, 0), 0.5, color='blue', fill=False)
-circle2 = Circle((3, 0), 0.5, color='red', fill=False)
+circle1 = Circle((-5, 0), 0.5, color='blue', fill=False)
+circle2 = Circle((5, 0), 0.5, color='red', fill=False)
 
 # Add circles to the plot
 ax.add_patch(circle1)
@@ -18,19 +18,20 @@ ax.add_patch(circle2)
 
 # Animation function
 def animate(i):
-    # Increase radius of both circles
-    new_radius = i / 10.0
+    # Slower radius growth
+    new_radius = 0.5 + i / 50.0  # Reduced the growth rate
     circle1.set_radius(new_radius)
     circle2.set_radius(new_radius)
     
-    # Move the circles towards each other
-    circle1.set_center((-3 + i / 20.0, 0))
-    circle2.set_center((3 - i / 20.0, 0))
+    # Move the circles towards each other, half overlap
+    max_overlap_distance = 2  # Maximum overlap at half distance
+    circle1.set_center((-5 + (i / 100.0) * max_overlap_distance, 0))
+    circle2.set_center((5 - (i / 100.0) * max_overlap_distance, 0))
     
     return circle1, circle2
 
-# Create animation
-anim = FuncAnimation(fig, animate, frames=100, interval=50)
+# Create animation with repeat=True to loop
+anim = FuncAnimation(fig, animate, frames=200, interval=100, repeat=True)
 
 # Show the animation
 plt.show()
